f1f7383495faf4790f7fc82b65621e1541835771
Max Jänicke <max.jaenicke@gmx.de>
2020-08-23 11:49:37 +0200

    add date functions and quotes

---
+++ src/components/shared/time.js
@@ -0,0 +1,47 @@
+Date.prototype.getWeek = () => {
+  var firstOfJanuary = new Date(this.getFullYear(), 0, 1);
+  return Math.ceil(
+    ((this - firstOfJanuary) / 86400000 + firstOfJanuary.getDay() + 1) / 7
+  );
+};
+
+const createDate = () => {
+  const date = new Date();
+  date.setHours(date.getHours() + 2); // offset for german summertime
+  return date;
+};
+
+const isToday = (date) => {
+  const inputDate = new Date(date);
+  const currentDate = createDate();
+
+  return (
+    inputDate.getDate() === currentDate.getDate() &&
+    inputDate.getMonth() === currentDate.getMonth() &&
+    inputDate.getFullYear() === currentDate.getFullYear()
+  );
+};
+
+const isCurrentWeek = (date) => {
+  const inputDate = new Date(date);
+  const currentDate = createDate();
+
+  return inputDate.getWeek() === currentDate.getWeek();
+};
+
+const isCurrentMonth = (date) => {
+  const inputDate = new Date(date);
+  const currentDate = createDate();
+
+  return (
+    inputDate.getMonth() === currentDate.getMonth() &&
+    inputDate.getFullYear() === currentDate.getFullYear()
+  );
+};
+
+const isCurrentYear = (date) => {
+  const inputDate = new Date(date);
+  const currentDate = createDate();
+
+  return inputDate.getFullYear() === currentDate.getFullYear();
+};
