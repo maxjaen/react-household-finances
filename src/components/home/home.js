0326fec021aed244f3c82c5dede82654800aa48d
Max Jänicke <max.jaenicke@gmx.de>
2020-08-23 15:32:21 +0200

    add home component

---
+++ src/components/home/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../styles/home/home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="block-center">
+        <div className="title"></div>
+        <div className="amount"></div>
+      </div>
+      <div className="block-bottom"></div>
+    </div>
+  );
+}
+
+export default Home;
